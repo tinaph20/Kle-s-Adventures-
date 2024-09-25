1c3e90add9a89479d78d5942d4fd67d9f0968412
tina <113174710+tinaph20@users.noreply.github.com>
2024-09-24 23:10:10 -0700

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Player player;
+    // Start is called before the first frame update
+    [SerializeField]
+    float MoveSpeed = 1f;
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        Debug.Log("Enemy Created");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = 
+            Vector2.MoveTowards(transform.position, player.transform.position, Time.deltaTime * MoveSpeed);
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("Enemy Destroyed");
+    }
+}
